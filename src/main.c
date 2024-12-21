f1d1993ab9fe4e5e4e278b21fa8dde0f51a32ca8
Mrbit <hi.mrbit@gmail.com>
2024-12-21 20:51:37 +0100

    philo v1

--- src/main.c
+++ src/main.c
@@ -1,116 +1,37 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+#include "../include/philo.h"
 
-typedef struct s_thread_data
+int	start_philos(pthread_t *threads, t_info *info, t_philo *philo)
 {
-    int philo_number ;
-    int *forks ;
-    int time_die ; 
-    int time_eat ;
-    int time_sleep ;
-    int total_forks ; 
-} t_thread_data; 
+	int	i;
 
-void philo_sleep(int philo_number , int time_sleep)
-{
-    printf("philo number ; %d is sleeping \n" , philo_number); 
-    usleep(time_sleep);
+	i = 0;
+	while (i < info->num_philos)
+	{
+		if (pthread_create(&threads[i], NULL, philo_life, &philo[i]))
+		{
+			write(2, "Pthread create error\n", 22);
+			return (1);
+		}
+		usleep(50);
+		i++;
+	}
+	return (0);
 }
 
-void philo_eat(t_thread_data *philo_data )
+int	main(int argc, char **argv)
 {
+	t_philo		*philo;
+	t_info		*info;
+	pthread_t	*threads;
 
-    int philo_number ;
-    int *forks  ;
-    int time_eat ;
-    int total_forks ;
-    int time_sleep  ;
-
-    philo_number =   philo_data -> philo_number    ;
-    time_eat  =      philo_data -> time_eat    ;
-    total_forks =   philo_data  -> total_forks    ;
-    time_sleep  =   philo_data  -> time_sleep     ;
-
-    if (philo_number == 0)
-    {
-        forks[philo_number]  =   0 ;
-        forks[total_forks - 1] = 0 ;  
-    }
-    else if (philo_number == total_forks - 1)
-    {
-        forks[philo_number]  =   0 ;
-        forks[0] = 0 ;  
-    }
-    else 
-    {
-        forks[philo_number]  =   0 ;
-        forks[philo_number - 1 ] = 0 ; 
-    }
-    //take_two_forks ; 
-    usleep(time_eat) ;
-
-    if (philo_number == 0)
-    {
-        forks[philo_number]  =   1 ;
-        forks[total_forks - 1] = 1 ;  
-    }
-    else if (philo_number == total_forks - 1)
-    {
-        forks[philo_number]  =   1 ;
-        forks[0] = 1 ;  
-    }
-    else 
-    {
-        forks[philo_number]  =   1 ;
-        forks[philo_number - 1 ] = 1 ; 
-
-    }
-    philo_sleep( philo_number , time_sleep);
-
-}
-
-void Mr_philosopher(struct thread_data *philo_data )
-{
-    int number_of_times_eaten ;
-
-    number_of_times_eaten =  0 ; 
-    
-    philo_eat(philo_data) ; 
-
-}
-
-
-/*
-    int philo_number ;
-    int *forks ;
-    int time_die ; 
-    int time_eat ;
-    int time_sleep ;
-    int total_forks ; 
-*/
-
-int main(int argc , char **argv , char **env)
-{
-   //   1-number_of_philosophers    2-time_to_die    3-time_to_eat     4-time_to_sleep  5-[number_of_times_each_philosopher_must_eat]
-
-   t_thread_data *philo_data ; 
-   int i; 
-
-   if ( argc != 5 && argc != 6   )
-   {
-        printf("Number of arguments invalid\n");
-        exit(EXIT_FAILURE);
-    }
-    i = 0 ;
-    philo_data = malloc( sizeof(t_thread_data) );
-    philo_data->time_die = atoi(argv[2]) ; 
-    philo_data -> time_eat  =  atoi( argv[3])  ; 
-    philo_data-> time_sleep  =  atoi(argv[4])  ;
-    philo_data-> total_forks =   atoi(argv[1]) ;  
-    philo_data-> forks  = malloc(  sizeof(int)  *  ( philo_data->total_forks )     ) ;
-    
-    
-    philo_data->philo_number =  i ; 
-
+	info = malloc(sizeof(t_info));
+	if (!info || !save_args(argc, argv, info) || init_info(info))
+		return (1);
+	philo = init_philo(info);
+	threads = malloc(info->num_philos * sizeof(pthread_t));
+	if (!philo || !threads || start_philos(threads, info, philo))
+		return (1);
+	check_philos_death(philo, info);
+	kill_philos(threads, info, philo);
+	return (0);
 }
