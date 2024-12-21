5e23980cddf651cc06f20db02cfde208995921c0
Abdellah Charik <acharik@e1r2p10.1337.ma>
2024-12-22 00:29:46 +0100

    v1

--- includes/init.h
+++ includes/init.h
@@ -1,4 +1,14 @@
-
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: acharik <acharik@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/22 00:18:38 by acharik           #+#    #+#             */
+/*   Updated: 2024/12/22 00:18:41 by acharik          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef INIT_H
 # define INIT_H
@@ -9,4 +19,4 @@ void	init_philos(t_engine *en, t_philo *philos, t_mutex *forks, char **argv);
 void	init_forks(t_engine *engine, t_mutex *forks, int count);
 void	init_engine(t_engine *engine, t_philo *philos, t_mutex *forks);
 
-#endif   /* INIT_H */
+#endif   
